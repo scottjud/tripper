edfada843cf96f33bfe482e1b9141cb0dfc01fca
Scott Judson <scottjud@gmail.com>
2014-07-25 17:24:54 -0400

    added validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :trip
 
-  validates :site, presence: true
+  validates :site, presence: true,
+                   length: { minimum: 2 }
+
+  validates :description, presence: true,
+                          length: { minimum: 4 }
 end
